a5c7ef4a07c439beb5c2ccab306d36f1cfbc4f42
smithc22atwit <smithc22@wit.edu>
2021-03-31 17:23:05 -0400

    added database integration to speaker class

--- CodeCampDesktopApp/Ui/SpeakerPage.Designer.cs
+++ CodeCampDesktopApp/Ui/SpeakerPage.Designer.cs
@@ -34,9 +34,6 @@ namespace CodeCampApp
             this.addNewSpeaker = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.SpeakerLabel = new System.Windows.Forms.Label();
-            this.NameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.EmailColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.PhoneNumberCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,10 +41,9 @@ namespace CodeCampApp
             // 
             this.addNewSpeaker.BackColor = System.Drawing.Color.White;
             this.addNewSpeaker.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.addNewSpeaker.Location = new System.Drawing.Point(1302, 92);
-            this.addNewSpeaker.Margin = new System.Windows.Forms.Padding(2);
+            this.addNewSpeaker.Location = new System.Drawing.Point(1953, 144);
             this.addNewSpeaker.Name = "addNewSpeaker";
-            this.addNewSpeaker.Size = new System.Drawing.Size(225, 34);
+            this.addNewSpeaker.Size = new System.Drawing.Size(338, 53);
             this.addNewSpeaker.TabIndex = 3;
             this.addNewSpeaker.Text = "Add New Speaker";
             this.addNewSpeaker.UseVisualStyleBackColor = false;
@@ -65,10 +61,6 @@ namespace CodeCampApp
             dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.NameColumn,
-            this.EmailColumn,
-            this.PhoneNumberCol});
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
             dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -77,52 +69,36 @@ namespace CodeCampApp
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
-            this.dataGridView1.Location = new System.Drawing.Point(104, 131);
+            this.dataGridView1.Location = new System.Drawing.Point(156, 205);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1423, 758);
+            this.dataGridView1.Size = new System.Drawing.Size(2134, 1184);
             this.dataGridView1.TabIndex = 4;
             // 
             // SpeakerLabel
             // 
             this.SpeakerLabel.AutoSize = true;
             this.SpeakerLabel.Font = new System.Drawing.Font("Segoe UI Semibold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SpeakerLabel.Location = new System.Drawing.Point(748, 72);
+            this.SpeakerLabel.Location = new System.Drawing.Point(1122, 112);
+            this.SpeakerLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.SpeakerLabel.Name = "SpeakerLabel";
-            this.SpeakerLabel.Size = new System.Drawing.Size(167, 54);
+            this.SpeakerLabel.Size = new System.Drawing.Size(269, 86);
             this.SpeakerLabel.TabIndex = 5;
             this.SpeakerLabel.Text = "Speaker";
             // 
-            // NameColumn
-            // 
-            this.NameColumn.HeaderText = "Name";
-            this.NameColumn.MinimumWidth = 6;
-            this.NameColumn.Name = "NameColumn";
-            // 
-            // EmailColumn
-            // 
-            this.EmailColumn.HeaderText = "Email";
-            this.EmailColumn.MinimumWidth = 6;
-            this.EmailColumn.Name = "EmailColumn";
-            // 
-            // PhoneNumberCol
-            // 
-            this.PhoneNumberCol.HeaderText = "Phone Number";
-            this.PhoneNumberCol.MinimumWidth = 6;
-            this.PhoneNumberCol.Name = "PhoneNumberCol";
-            // 
             // SpeakerPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.ClientSize = new System.Drawing.Size(1539, 844);
+            this.ClientSize = new System.Drawing.Size(2308, 1319);
             this.Controls.Add(this.SpeakerLabel);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.addNewSpeaker);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.Name = "SpeakerPage";
             this.Text = "SpeakerPage";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
@@ -136,8 +112,5 @@ namespace CodeCampApp
         private System.Windows.Forms.Button addNewSpeaker;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label SpeakerLabel;
-        private System.Windows.Forms.DataGridViewTextBoxColumn NameColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn EmailColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn PhoneNumberCol;
     }
 }
--- CodeCampDesktopApp/Ui/SpeakerPage.cs
+++ CodeCampDesktopApp/Ui/SpeakerPage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,19 +13,186 @@ namespace CodeCampApp
 {
     public partial class SpeakerPage : Form
     {
+        private DataTable _dt;
+        string connectionString = @"Server=mydb.c6botwup9amq.us-east-2.rds.amazonaws.com;Database=projectconference;Uid=root;Pwd=password123;convert zero datetime=True";
         public SpeakerPage()
         {
             InitializeComponent();
+
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+
+                var stm = "select * from speaker";
+                var cmd = new MySqlCommand(stm, mysqlCon);
+
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                DataTable dt = new DataTable();
+                _dt = dt;
+                dt.Columns.Add("id", typeof(int));
+                dt.Columns.Add("name", typeof(string));
+                dt.Columns.Add("email", typeof(string));
+                dt.Columns.Add("phone_number", typeof(string));
+                dt.Columns.Add("edit", typeof(string));
+                dt.Columns.Add("delete", typeof(string));
+                dt.Columns.Add("save", typeof(string));
+                dt.Columns.Add("cancel", typeof(string));
+
+                while (rdr.Read())
+                {
+                    DataRow dr = dt.NewRow();
+                    dr["id"] = rdr.GetInt32(0);
+                    dr["name"] = rdr.GetString(1);
+                    dr["email"] = rdr.GetString(2);
+                    dr["phone_number"] = rdr.GetString(3);
+                    dr["edit"] = "1";
+                    dr["delete"] = "1";
+                    dr["save"] = "1";
+                    dr["cancel"] = "1";
+                    dt.Rows.Add(dr);
+                }
+                dataGridView1.Columns.Add("id", "ID");
+                dataGridView1.Columns.Add("name", "Name");
+                dataGridView1.Columns.Add("email", "Email");
+                dataGridView1.Columns.Add("phone_number", "Phone Number");
+                dataGridView1.Columns.Add("edit", "Edit");
+                dataGridView1.Columns.Add("delete", "Delete");
+                dataGridView1.Columns.Add("save", "Save");
+                dataGridView1.Columns.Add("cancel", "Cancel");
+                dataGridView1.Columns["name"].ReadOnly = true;
+                dataGridView1.Columns["email"].ReadOnly = true;
+                dataGridView1.Columns["phone_number"].ReadOnly = true;
+                dataGridView1.Columns["id"].Visible = false;
+                dataGridView1.Columns["save"].Visible = false;
+                dataGridView1.Columns["cancel"].Visible = false;
+                dataGridView1.AllowUserToAddRows = false;
+                string[] lst_dados = new string[4];
+
+                foreach (DataRow dr1 in dt.Rows)
+                {
+                    lst_dados[0] = dr1["id"].ToString();
+                    lst_dados[1] = dr1["name"].ToString();
+                    lst_dados[2] = dr1["email"].ToString();
+                    lst_dados[3] = dr1["phone_number"].ToString();
+                    this.dataGridView1.Rows.Add(lst_dados);
+
+                    if (dr1["edit"].ToString() == "1")
+                    {
+                        DataGridViewButtonCell btn = new DataGridViewButtonCell();
+                        btn.Value = "Edit";
+                        btn.UseColumnTextForButtonValue = true;
+                        this.dataGridView1.Rows[this.dataGridView1.Rows.Count - 1].Cells["edit"] = btn;
+                    }
+                    if (dr1["delete"].ToString() == "1")
+                    {
+                        DataGridViewButtonCell btn = new DataGridViewButtonCell();
+                        btn.Value = "Delete";
+                        btn.UseColumnTextForButtonValue = true;
+                        this.dataGridView1.Rows[this.dataGridView1.Rows.Count - 1].Cells["delete"] = btn;
+                    }
+                    if (dr1["save"].ToString() == "1")
+                    {
+                        DataGridViewButtonCell btn = new DataGridViewButtonCell();
+                        btn.Value = "Save";
+                        btn.UseColumnTextForButtonValue = true;
+                        this.dataGridView1.Rows[this.dataGridView1.Rows.Count - 1].Cells["save"] = btn;
+                    }
+                    if (dr1["cancel"].ToString() == "1")
+                    {
+                        DataGridViewButtonCell btn = new DataGridViewButtonCell();
+                        btn.Value = "Cancel";
+                        btn.UseColumnTextForButtonValue = true;
+                        this.dataGridView1.Rows[this.dataGridView1.Rows.Count - 1].Cells["cancel"] = btn;
+                    }
+
+                }
+
+                mysqlCon.Close();
+            }
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-
+            Console.WriteLine("Hello");
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            handleButtonClick(sender, e, "edit");
+            handleButtonClick(sender, e, "delete");
+            handleButtonClick(sender, e, "save");
+            handleButtonClick(sender, e, "cancel");
+        }
 
+        private void handleButtonClick(object sender, DataGridViewCellEventArgs e, string buttonName)
+        {
+            if (e.ColumnIndex == this.dataGridView1.Columns[buttonName].Index)
+            {
+                if (buttonName == "edit")
+                {
+                    dataGridView1.Columns[4].HeaderCell.Value = "Save";
+                    dataGridView1.Columns[5].HeaderCell.Value = "Cancel";
+                    dataGridView1.Columns["name"].ReadOnly = false;
+                    dataGridView1.Columns["email"].ReadOnly = false;
+                    dataGridView1.Columns["phone_number"].ReadOnly = false;
+                    dataGridView1.Columns["edit"].Visible = false;
+                    dataGridView1.Columns["delete"].Visible = false;
+                    dataGridView1.Columns["save"].Visible = true;
+                    dataGridView1.Columns["cancel"].Visible = true;
+                }
+
+                if (buttonName == "delete")
+                {
+                    Console.WriteLine("delete");
+                }
+
+                if (buttonName == "save")
+                {
+                    dataGridView1.Columns[4].HeaderCell.Value = "Edit";
+                    dataGridView1.Columns[5].HeaderCell.Value = "Delete";
+                    dataGridView1.Columns["name"].ReadOnly = true;
+                    dataGridView1.Columns["email"].ReadOnly = true;
+                    dataGridView1.Columns["phone_number"].ReadOnly = true;
+                    dataGridView1.Columns["edit"].Visible = true;
+                    dataGridView1.Columns["delete"].Visible = true;
+                    dataGridView1.Columns["save"].Visible = false;
+                    dataGridView1.Columns["cancel"].Visible = false;
+
+                    // Query Database
+
+                    using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+                    {
+                        mysqlCon.Open();
+
+                        var stm = "update speaker set name = @newName, email = @newEmail, phone_number = @newPhoneNumber where speaker_id = @id";
+
+                        var cmd = new MySqlCommand(stm, mysqlCon);
+                        cmd.Parameters.AddWithValue("@newName", this.dataGridView1[1, e.RowIndex].Value.ToString());
+                        cmd.Parameters.AddWithValue("@newEmail", this.dataGridView1[2, e.RowIndex].Value.ToString());
+                        cmd.Parameters.AddWithValue("@newPhoneNumber", this.dataGridView1[3, e.RowIndex].Value.ToString());
+                        cmd.Parameters.AddWithValue("@id", Int32.Parse(this.dataGridView1[0, e.RowIndex].Value.ToString()));
+                        cmd.ExecuteNonQuery();
+
+                        mysqlCon.Close();
+                    }
+                }
+
+                if (buttonName == "cancel")
+                {
+
+                     dataGridView1.Columns[4].HeaderCell.Value = "Edit";
+                     dataGridView1.Columns[5].HeaderCell.Value = "Delete";
+                     dataGridView1.Columns["name"].ReadOnly = true;
+                     dataGridView1.Columns["email"].ReadOnly = true;
+                     dataGridView1.Columns["phone_number"].ReadOnly = true;
+                     dataGridView1.Columns["edit"].Visible = true;
+                     dataGridView1.Columns["delete"].Visible = true;
+                     dataGridView1.Columns["save"].Visible = false;
+                     dataGridView1.Columns["cancel"].Visible = false;
+                }
+            }
         }
 
         private void label2_Click(object sender, EventArgs e)
